534e413a6fd515a92f84a81b6037fe3f05306062
ibossbabu <boss160250@gmail.com>
2025-04-14 20:55:14 +0700

    fix formatter

--- nvim/.config/nvim/lua/plugins/guard.lua
+++ nvim/.config/nvim/lua/plugins/guard.lua
@@ -5,7 +5,7 @@ return {
     dependencies = { 'nvimdev/guard-collection' },
     config = function()
       local ft = require("guard.filetype")
-      ft("c,cpp,json"):fmt("clang-format")
+      ft("c,cpp"):fmt("clang-format")
     end,
   },
   vim.keymap.set('n', '<leader>nf', vim.lsp.buf.format, {})
