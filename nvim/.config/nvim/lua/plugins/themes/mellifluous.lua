2dbbd78c8271a0e2c4658d148ffce64fab6d3bd0
ibossbabu <boss160250@gmail.com>
2025-04-18 15:12:13 +0700

    add themes

---
+++ nvim/.config/nvim/lua/plugins/themes/mellifluous.lua
@@ -0,0 +1,34 @@
+return {
+  "ramojus/mellifluous.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("mellifluous").setup({
+      mellifluous = {
+        highlight_overrides = {
+          dark = function(highlighter, colors)
+            highlighter.set("VISUAL", { bg = "#493a4c" })
+          end,
+        },
+        color_overrides = {
+          dark = {
+            bg = function(bg)
+              return bg:lightened(2)
+            end,
+            colors = function(colors)
+              return {
+                main_keywords = "#955ae7",
+                functions = "#576ddb",
+                strings = "#2a9292",
+                constants = "#a06e3b",
+                other_keywords = "#be4678",
+                -- operators = colors.functions:desaturated(10),
+                -- color based on Atelier_CaveDark
+              }
+            end,
+          },
+        },
+      },
+    })
+  end
+}
