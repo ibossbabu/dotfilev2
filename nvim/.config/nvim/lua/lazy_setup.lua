a37c7e4b9f24ae46791cd3559877b849bb7778b6
ibossbabu <boss160250@gmail.com>
2025-04-19 23:51:28 +0700

    update

--- nvim/.config/nvim/lua/lazy_setup.lua
+++ nvim/.config/nvim/lua/lazy_setup.lua
@@ -4,7 +4,6 @@ require("lazy").setup({
     { import = "plugins" },
     { import = "plugins/themes" },
   },
-  -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
--- nvim/.config/nvim/lua/plugins/themes/nightfox.lua
+++ nvim/.config/nvim/lua/plugins/themes/nightfox.lua
@@ -3,5 +3,15 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("nightfox").setup({
+      palettes = {
+        nightfox = {
+          bg1 = "#000000", -- Set the main background to black
+        },
+      },
+      options = {
+        transparent = false, -- Ensure transparency is disabled
+      },
+    })
   end
 }
