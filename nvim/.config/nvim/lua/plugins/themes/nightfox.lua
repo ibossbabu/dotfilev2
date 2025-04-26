be9b98f6d8a7ddee54d022d608869c4727eeab05
ibossbabu <boss160250@gmail.com>
2025-04-26 22:07:18 +0700

    add duskfox

--- nvim/.config/nvim/lua/plugins/themes/nightfox.lua
+++ nvim/.config/nvim/lua/plugins/themes/nightfox.lua
@@ -8,6 +8,18 @@ return {
         nightfox = {
           bg1 = "#000000", -- Set the main background to black
         },
+        duskfox = {
+          bg0 = "#000000",  -- Set the main background to black
+          bg1 = "#000000",  -- Current line background
+          bg2 = "#1d1d2b",  -- Alt backgrounds (floats, statusline, ...)
+          bg3 = "#121820",  -- 55% darkened from stock
+          sel0 = "#131b24", -- 55% darkened from stocks
+        },
+      },
+      specs = {
+        duskfox = {
+          inactive = "#090909", -- Slightly lighter than black background
+        },
       },
       options = {
         transparent = false, -- Ensure transparency is disabled
